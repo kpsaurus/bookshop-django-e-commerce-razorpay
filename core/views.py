4347c8a8cfd998b1d8ef06ea8560de3d5ef3ef5b
KP <59389473+kpsaurus@users.noreply.github.com>
2021-12-03 22:00:52 +0530

    Logout function added

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+from django.urls import reverse
+
+
 def index(request):
     return render(request, 'core/index.html')
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect(reverse('index'))
