656be76cdf078e63539d4e771c0d85a26a0b29f2
KP <59389473+kpsaurus@users.noreply.github.com>
2021-12-08 17:05:56 +0530

    Added search_books function.
    Added search_books function.

--- core/views.py
+++ core/views.py
@@ -1,14 +1,32 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 from django.urls import reverse
+from .models import Book, Category
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    books = Book.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'books': books,
+        'categories': categories,
+    })
 
 
 def logout(request):
     auth_logout(request)
     return redirect(reverse('index'))
+
+
+def search_books(request):
+    category = request.GET['category']
+    if category is None:
+        books = Book.objects.all()
+    else:
+        books = Book.objects.filter(category__category=category).all()
+    return render(request, 'core/books.html', {
+        'books': books,
+    })
