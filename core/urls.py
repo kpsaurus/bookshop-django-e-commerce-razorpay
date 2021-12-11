6a1df83a7b0a014ea43afb43a0f997fe7c3dfc60
KP <59389473+kpsaurus@users.noreply.github.com>
2021-12-11 17:37:55 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, logout, BookDetails, make_order
+from .views import index, logout, BookDetails, make_order, gateway_response
 
 urlpatterns = [
     path('', index, name='index'),
     path('book/<pk>', BookDetails.as_view(), name='book-details'),
     path('make-order/', make_order, name='make-order'),
+    path('gateway-response', gateway_response, name='gateway-response'),
     path('logout', logout, name='sign-out')
 ]
