00932c80575ed26fea1a8693b5ed780126a9842d
KP <59389473+kpsaurus@users.noreply.github.com>
2021-12-11 12:41:02 +0530

    Update urls.py
    Added make-order route.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, logout, BookDetails
+from .views import index, logout, BookDetails, make_order
 
 urlpatterns = [
     path('', index, name='index'),
     path('book/<pk>', BookDetails.as_view(), name='book-details'),
+    path('make-order/', make_order, name='make-order'),
     path('logout', logout, name='sign-out')
 ]
