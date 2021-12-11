890e258ca6668bd70b2a333e1c276255960c4b1a
KP <59389473+kpsaurus@users.noreply.github.com>
2021-12-11 17:33:38 +0530

    Update forms.py
    Added PaymentGatewayResponse for payment gateway response.

--- core/forms.py
+++ core/forms.py
@@ -3,3 +3,9 @@ from django import forms
 
 class MakeOrderForm(forms.Form):
     selected_product = forms.IntegerField()
+
+
+class PaymentGatewayResponse(forms.Form):
+    razorpay_payment_id = forms.CharField(max_length=50, required=True)
+    razorpay_order_id = forms.CharField(max_length=50, required=True)
+    razorpay_signature = forms.CharField(max_length=500, required=True)
